f827774690257b7da9be3eefafb44782a9399b9f
man20820 <fatkhurrahmandjogja@gmail.com>
2022-12-26 03:12:18 +0700

    modify auto deployment

--- deployments/docker/container.sh
+++ deployments/docker/container.sh
@@ -3,5 +3,6 @@ docker rmi $(docker images | grep '{{ IMAGE_NAME_RAW }}')
 docker run -d --name {{ APP_NAME }} \
     -e MONGO_URI="{{ MONGO_URI }}" \
     -e TELEGRAM_BOT_TOKEN="{{ TELEGRAM_BOT_TOKEN }}" \
+    -e TELEGRAM_REPORT_CHAT_ID="{{ TELEGRAM_REPORT_CHAT_ID }}" \
     --restart always \
     {{ IMAGE_NAME }}
